d57e3fa1e633b83e343f1f5852973d992f6acf1b
EC2 Default User <ec2-user@ip-172-31-12-40.ec2.internal>
2023-10-20 19:22:27 +0000

    added unit testing

--- list-buckets.py
+++ list-buckets.py
@@ -1,5 +1,23 @@
 import boto3
 import json
+import argparse
+import unittest
+import unit_test
+
+unit_test = unit_test.TestMyCode()
+
+from unittest.mock import Mock, patch
+from io import BytesIO
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--storage', choices=['S3', 'Dynamodb'])
+parser.add_argument('--resource', type=str)
+
+args = parser.parse_args()
+
+storage = args.storage
+resource = args.resource
 
 
 session = boto3.Session()
@@ -8,8 +26,10 @@ dynamodb_client = session.client('dynamodb')
 
 bucket_name_2 = 'usu-cs5250-global-requests'
 bucket_name_3 = 'usu-cs5250-global-web'
-table_name = 'widgets'
+table_name = resource
 
+print(storage)
+print(resource)
 
 objects = s3_client.list_objects_v2(Bucket=bucket_name_2)
 
@@ -21,26 +41,26 @@ if 'Contents' in objects:
             print(obj['Key'])
             object_content = json.loads(response)
             if(object_content['type'] == "create"):
-                # key  = f'widgets/{object_content["owner"].lower().replace(" ", "-")}/{object_content["widgetId"]}'
-                # response = s3_client.put_object(Bucket=bucket_name_3, Key=key, Body=json.dumps(object_content))
-                
-                data_to_store = {
-                    'widget_id': {'S': object_content['widgetId']},
-                    'id': {'S': object_content['requestId']},
-                    'owner': {'S': object_content['owner']},
-                    'label': {'S': object_content['label']},
-                    'description': {'S': object_content['description']},
-                }
-                
+                if(storage == 'S3'):
+                    key  = f'widgets/{object_content["owner"].lower().replace(" ", "-")}/{object_content["widgetId"]}'
+                    response = s3_client.put_object(Bucket=bucket_name_3, Key=key, Body=json.dumps(object_content))
+                    unit_test.unit_test_s3(object_content, obj['Key'], bucket_name_2, bucket_name_3, s3_client)
+                elif(storage == 'Dynamodb'):
+                    data = {
+                        'widget_id': {'S': object_content['widgetId']},
+                        'id': {'S': object_content['requestId']},
+                        'owner': {'S': object_content['owner']},
+                        'label': {'S': object_content['label']},
+                        'description': {'S': object_content['description']},
+                    }
+                    
+                    for key in object_content['otherAttributes']:
+                        data[key['name']]=  {'S': key['value']}
+                    
+                    response = dynamodb_client.put_item(TableName=table_name, Item=data)
+                    unit_test.unit_test_dynamodb(data, obj['Key'], table_name, bucket_name_3, dynamodb_client, s3_client)
 
-                for key in object_content['otherAttributes']:
-                    data_to_store[key['name']]=  {'S': key['value']}
-                
-                print(data_to_store)
-                
-                response = dynamodb_client.put_item(TableName=table_name, Item=data_to_store)
-
-                
+            
         except Exception as e:
             print(f'Error: {e}')
             
---
+++ unit_test.py
@@ -0,0 +1,70 @@
+import unittest
+from unittest.mock import patch, Mock
+import io
+import boto3
+import json
+
+
+class TestMyCode(unittest.TestCase):
+
+    def unit_test_s3(self, expected_result, key, resource, dist_bucket, s3_client):
+        print(resource, key)
+        response = s3_client.get_object(Bucket=resource, Key=key)
+        response = response['Body'].read()
+        try:
+            result = json.loads(response)
+            self.assertEqual(result, expected_result)
+            
+            test_report = {
+                "type": "create",
+                "owner": expected_result['owner'],
+                "widgetId": expected_result['widgetId'],
+                "status": "successful"
+            }
+            s3_client.put_object(Bucket=dist_bucket, Key=f"test_report_s3/{key}", Body=json.dumps(test_report))
+        except Exception as e:
+            print(f'Error: {e}')
+            test_report = {
+                "type": "create",
+                "owner": expected_result['owner'],
+                "widgetId": expected_result['widgetId'],
+                "status": "failed"
+            }
+            s3_client.put_object(Bucket=dist_bucket, Key=f"test_report_s3/{key}", Body=json.dumps(test_report))
+        
+    def unit_test_dynamodb(self, expected_result, key, resource, dist_bucket, dynamodb, s3_client):
+        key_condition = '#id = :id' 
+        values = {':id': {'S': f"{expected_result['id']['S']}"}}
+        names = {'#id': 'id'}
+        
+        response = dynamodb.query(
+            TableName=resource,
+            KeyConditionExpression=key_condition,
+            ExpressionAttributeValues=values,
+            ExpressionAttributeNames=names
+        )
+        
+        result = response['Items']
+        
+        test_report = {
+                "type": "create",
+                "status": "successful"
+            }
+        
+        try:
+            self.assertEqual(result[0], expected_result)
+            test_report = {
+                "type": "create",
+                "status": "successful"
+            }
+            s3_client.put_object(Bucket=dist_bucket, Key=f"test_report_dynamodb/{key}", Body=json.dumps(test_report))
+        except Exception as e:
+            print(f'Error: {e}')
+            test_report = {
+                "type": "create",
+                "status": "failed"
+            }
+            s3_client.put_object(Bucket=dist_bucket, Key=f"test_report_dynamodb/{key}", Body=json.dumps(test_report))
+            
+            
+
